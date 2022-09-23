7ab9f9b69e2054fbeb1ffbcc822939334a3aa9e2
Steeve Lennmark <steevel@handeldsbanken.se>
2022-09-23 19:10:18 +0300

    Add support for utilizing tax loophole

--- grape-node-autocompounder.ts
+++ grape-node-autocompounder.ts
@@ -5,8 +5,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const grapeNodeCost = parseInt(process.env.GRAPE_NODE_COST as string, 10);
+const bufferGrapes = parseInt(process.env.COMPOUND_BUFFER_GRAPES as string, 10);
 const totalGrapeNodeCost =
-  grapeNodeCost * parseInt(process.env.COMPOUND_AT_NODES as string, 10);
+  grapeNodeCost * parseInt(process.env.COMPOUND_AT_NODES as string, 10) +
+  bufferGrapes;
 const compoundInterval = parseInt(process.env.COMPOUND_INTERVAL as string, 10);
 
 const abi = [
@@ -49,7 +51,12 @@ async function grapeCompounder() {
       +(await contract.getTotalRewards(process.env.WALLET_ADDRESS as string)) /
       1e18;
     if (rewards < totalGrapeNodeCost) {
-      log(true, `Not enough pending to compound: ${rewards}`);
+      log(
+        true,
+        `Not enough to compound: ${rewards.toFixed(
+          6
+        )}, needed: ${totalGrapeNodeCost}`
+      );
       return;
     }
   } catch {
