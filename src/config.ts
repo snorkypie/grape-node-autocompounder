b9df537fe2a57a00d25ba8102474c816bcf12fbd
Steeve Lennmark <steevel@handeldsbanken.se>
2022-09-28 09:02:00 +0300

    Add config validation and return a typed config object

---
+++ src/config.ts
@@ -0,0 +1,129 @@
+import dotenv from 'dotenv';
+import { ethers } from 'ethers';
+
+dotenv.config();
+
+type Config = {
+  privKey: string;
+  address: string;
+  compoundAtNodes: number;
+  bufferGrapes: number;
+  contract: string;
+  rpcURL: string;
+  nodeCost: number;
+  compoundInterval: number;
+};
+
+type Variable = {
+  input: string;
+  output: string;
+  validation?: (key: any) => boolean;
+  type?: string;
+  defaultValue?: any;
+  assign?: (key: any) => [[string, any]];
+};
+
+const variables: Variable[] = [
+  {
+    input: 'PRIVATE_KEY',
+    output: 'privKey',
+    validation: (key: string) => /^[a-f0-9]{64}$/i.test(key),
+    assign: (key: string) => [['address', new ethers.Wallet(key).address]],
+  },
+  {
+    input: 'COMPOUND_AT_NODES',
+    output: 'compoundAtNodes',
+    type: 'integer',
+    defaultValue: 1,
+  },
+  {
+    input: 'COMPOUND_BUFFER_GRAPES',
+    output: 'bufferGrapes',
+    validation: (value: number) => value >= 0 && value < 50,
+    type: 'number',
+    defaultValue: 0,
+  },
+  {
+    input: 'CONTRACT_ADDRESS',
+    output: 'contract',
+    defaultValue: '0xd77b0756be406a6a78d47285edd59234d781d568',
+  },
+  {
+    input: 'RPC_URL',
+    output: 'rpcURL',
+    defaultValue: 'https://api.avax.network/ext/bc/C/rpc',
+  },
+  {
+    input: 'GRAPE_NODE_COST',
+    output: 'nodeCost',
+    type: 'number',
+    defaultValue: 50,
+  },
+  {
+    input: 'COMPOUND_INTERVAL',
+    output: 'compoundInterval',
+    type: 'number',
+    defaultValue: 10,
+  },
+];
+
+function getValue(
+  name: string,
+  type: string,
+  value: string | undefined,
+  defaultValue: any
+): string | number {
+  if (typeof value === 'undefined') {
+    if (typeof defaultValue !== 'undefined') {
+      return defaultValue;
+    }
+    throw Error(`Value for '${name}' not supplied.`);
+  } else if (type === 'integer') {
+    const iValue = parseInt(value, 10);
+    if (isNaN(iValue)) {
+      throw Error(`${name} is not set to a valid integer: '${value}'`);
+    }
+    return iValue;
+  } else if (type === 'number') {
+    const iValue = Number(value);
+    if (isNaN(iValue)) {
+      throw Error(`${name} is not set to a valid number: '${value}'`);
+    }
+    return iValue;
+  }
+
+  return value;
+}
+
+let correctlyParsed = true;
+const config: Config = Object.fromEntries(
+  variables
+    .map(({ defaultValue, input, output, assign, type, validation }) => {
+      const origValue = process.env[input];
+      const value = getValue(input, type || 'string', origValue, defaultValue);
+      if (typeof validation !== 'undefined' && !validation(value)) {
+        console.error(`Incorrect configuration value set for '${input}'`);
+        correctlyParsed = false;
+      }
+
+      try {
+        return [
+          [output, value],
+          ...(typeof assign !== 'undefined' ? assign(value) : []),
+        ];
+      } catch (e: any) {
+        console.error(e.message);
+        correctlyParsed = false;
+      }
+
+      return [];
+    })
+    .flat()
+);
+process.exit(1);
+
+if (!correctlyParsed) {
+  process.exit(1);
+}
+
+export default config;
--- src/grape-node-autocompounder.ts
+++ src/grape-node-autocompounder.ts
@@ -1,28 +1,19 @@
 import { StaticJsonRpcProvider } from '@ethersproject/providers';
 import { ethers } from 'ethers';
-import dotenv from 'dotenv';
 
-dotenv.config();
+import config from './config';
 
-const grapeNodeCost = parseInt(process.env.GRAPE_NODE_COST as string, 10);
-const bufferGrapes = parseInt(process.env.COMPOUND_BUFFER_GRAPES as string, 10);
 const totalGrapeNodeCost =
-  grapeNodeCost * parseInt(process.env.COMPOUND_AT_NODES as string, 10) +
-  bufferGrapes;
-const compoundInterval = parseInt(process.env.COMPOUND_INTERVAL as string, 10);
+  config.nodeCost * config.compoundAtNodes + config.bufferGrapes;
 
 const abi = [
   'function compound() public',
   'function getTotalRewards(address) public view returns (uint256)',
 ];
 
-const provider = new StaticJsonRpcProvider(process.env.RPC_URL as string);
-const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
-const contract = new ethers.Contract(
-  process.env.CONTRACT_ADDRESS as string,
-  abi,
-  signer
-);
+const provider = new StaticJsonRpcProvider(config.rpcURL);
+const signer = new ethers.Wallet(config.privKey, provider);
+const contract = new ethers.Contract(config.contract, abi, signer);
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
@@ -47,9 +38,7 @@ const log = (status: boolean, ...args: any[]) => {
 async function grapeCompounder() {
   let rewards;
   try {
-    rewards =
-      +(await contract.getTotalRewards(process.env.WALLET_ADDRESS as string)) /
-      1e18;
+    rewards = +(await contract.getTotalRewards(config.address)) / 1e18;
     if (rewards < totalGrapeNodeCost) {
       log(
         true,
@@ -73,7 +62,7 @@ async function grapeCompounder() {
       return;
     }
 
-    const numNodes = Math.floor(rewards / grapeNodeCost);
+    const numNodes = Math.floor(rewards / config.nodeCost);
     log(
       false,
       `Successfully compounded ${numNodes} grape node${numNodes > 1 ? 's' : ''}`
@@ -88,6 +77,6 @@ async function grapeCompounder() {
 
   setTimeout(async function compoundTimeout() {
     await grapeCompounder();
-    setTimeout(compoundTimeout, compoundInterval * 1000);
-  }, compoundInterval * 1000);
+    setTimeout(compoundTimeout, config.compoundInterval * 1000);
+  }, config.compoundInterval * 1000);
 })();
